99d8320e8727cc991a13edcc3f8a6c7d21cb5a3b
root_hbx <huboxuan2004@gmail.com>
2025-04-28 02:06:24 +0800

    nits for first page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://root-hbx.github.io',
-	// base: '/opensat-doc',
+	base: '/opensat-doc',
 
 	integrations: [
 		starlight({
